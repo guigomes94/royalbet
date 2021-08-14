3cfe279470d616d36346c49fbebc5e9e4ea735e2
guigomes94 <guigomes94@outlook.com>
2021-08-14 16:51:43 -0300

    Estruturacao e config

---
+++ src/main/java/br/com/royalbet/RoaylBetApplication.java
@@ -0,0 +1,13 @@
+package br.com.royalbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoaylBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoaylBetApplication.class, args);
+	}
+
+}
