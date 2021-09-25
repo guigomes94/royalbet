4bfc01ab03fc56f39d4d77fa47f0021da81a674d
guigomes94 <guigomes94@outlook.com>
2021-09-25 16:24:12 -0300

    ajustes home, user, admin

--- src/main/java/br/com/royalbet/controllers/HomeController.java
+++ src/main/java/br/com/royalbet/controllers/HomeController.java
@@ -1,10 +1,16 @@
 package br.com.royalbet.controllers;
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.com.royalbet.models.User;
 
 @Controller
 @RequestMapping("/home")
@@ -15,5 +21,17 @@ public class HomeController {
         model.setViewName("/home");
         return model;
     }
+    
+    @GetMapping("/user/{id}")
+	public String findById(@PathVariable(value = "id") Long id, Model model, HttpSession session) {
+    	User logado = (User) session.getAttribute("user");
+		if (logado == null) {
+			model.addAttribute("mensagem", "Usuario não encontrado");
+			model.addAttribute("user", new User());
+		} else {
+			model.addAttribute("user", logado);
+		}
+		return "user/logado";
+	}
 
 }
--- src/main/java/br/com/royalbet/repositories/UserRepository.java
+++ src/main/java/br/com/royalbet/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package br.com.royalbet.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.royalbet.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByCpf(String cpf);
+
+    @Query("SELECT u FROM User u WHERE u.id != ?1")
+    List<User> findOthers(Long id);
 }
